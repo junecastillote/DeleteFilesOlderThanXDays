7c5a340bc121b9982a75641f82b30d7927970cbf
June Castillote <june.castillote@gmail.com>
2016-09-12 09:03:47 +0800

    1.0

--- DeleteFilesOlderThanXDays.ps1
+++ DeleteFilesOlderThanXDays.ps1
@@ -1,5 +1,5 @@
+$scriptVersion = "1.0"
 #http://shaking-off-the-cobwebs.blogspot.com/2016/09/DeleteFilesOlderThanXDays.html
-
 #Region Variables
 	$today = '{0:dd/MMM/yyyy hh:mm tt}' -f (Get-Date)
 	$script_root = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
@@ -7,9 +7,13 @@
 	$debugLog += $today
 	$debugLogFile = ($script_root + "\DeleteLog.txt")
 	$DirList = gc ($script_root + "\DirList.txt")
+	
+	#Change this value to your preferred number of days (in negative)
 	$HowManyDaysOld = "-15"
-	$DatetoDelete = (Get-Date).AddDays($HowManyDaysOld)
+	#Change boolean value to whether you want to receive an email report or not
 	$SendEmail = $true
+	
+	$DatetoDelete = (Get-Date).AddDays($HowManyDaysOld)
 #EndRegion
 
 foreach ($dirPath in $DirList) {
@@ -30,18 +34,18 @@ foreach ($dirPath in $DirList) {
 
 $debugLog | Out-File $debugLogFile
 
-#Region
-
-$mail_body = "Deleted IIS Logs older than [$($HowManyDaysOld)] days"
-$params = @{
-    Body = $mail_body
-    BodyAsHtml = $false
-    Subject = "MMG IIS Logs Maintenance $($today)"
-    From = "MMG PostMaster <Exchange-Admin@mmg.com>"
-	To = "gcp.messaging.exchangets@hpe.com"
-    SmtpServer = "cust11646-s.out.mailcontrol.com"
-	Attachment = $debugLogFile
-	
+#Region Mail
+if ($SendEmail -eq $true) {
+	$mail_body = "Deleted IIS Logs older than [$($HowManyDaysOld)] days"
+	$params = @{
+		Body = $mail_body
+		BodyAsHtml = $false
+		Subject = "IIS Logs Maintenance $($today)"
+		From = "PostMaster <Exchange-Admin@domain.com>"
+		To = "someone@domain.com"
+		SmtpServer = "smtp.server.here"
+		Attachment = $debugLogFile	
+		}
+	Send-MailMessage @params
 }
-
-Send-MailMessage @params
+#EndRegion
