8f0946fe05cd83f9b27fa4f73be1cbef65bb0152
June Castillote <june.castillote@gmail.com>
2019-01-28 10:50:59 +0800

    v1.1
    Added error handling, output result to CSV

--- DeleteFilesOlderThanXDays.ps1
+++ DeleteFilesOlderThanXDays.ps1
@@ -1,4 +1,4 @@
-$scriptVersion = "1.0"
+$scriptVersion = "1.1"
 #http://www.lazyexchangeadmin.com/2016/09/DeleteFilesOlderThanXDays.html
 #Region Variables
 	$today = '{0:dd/MMM/yyyy hh:mm tt}' -f (Get-Date)
@@ -6,32 +6,47 @@ $scriptVersion = "1.0"
 	$debugLog = @()
 	$debugLog += $today
 	$debugLogFile = ($script_root + "\DeleteLog.txt")
-	$DirList = gc ($script_root + "\DirList.txt")
+	$DirList = get-content ($script_root + "\DirList.txt")
+	$deleteLogCSVFile = ($script_root + "\DeleteLog.csv")
 	
 	#Change this value to your preferred number of days (in negative)
-	$HowManyDaysOld = "-15"
+	$HowManyDaysOld = "-0"
 	#Change boolean value to whether you want to receive an email report or not
-	$SendEmail = $true
-	
+	$SendEmail = $false	
 	$DatetoDelete = (Get-Date).AddDays($HowManyDaysOld)
 #EndRegion
 
+$resultLog = @()
 foreach ($dirPath in $DirList) {
 
 	#Write-Host $dirPath -ForegroundColor Green
-	$logFiles = Get-ChildItem $dirPath | Where-Object { $_.LastWriteTime -lt $DatetoDelete }
+	$logFiles = Get-ChildItem $dirPath #| Where-Object { $_.LastWriteTime -lt $DatetoDelete }
 	
 	if (($logFiles.Count) -gt 0) {
 	
 		foreach ($logFile in $logFiles) {
+		$temp = "" | Select-Object FileName,Successful
 		
-		Write-Host "Delete $($logFile.FullName)" -ForegroundColor Green
 		$debugLog += ($logFile.FullName)
-		Remove-Item -Path ($logFile.FullName) -Force -ErrorAction SilentlyContinue
+		$temp.FileName = $logFile.FullName
+
+		try {
+			Write-Host "Delete $($logFile.FullName).. " -ForegroundColor Green -NoNewline
+			Remove-Item -Path ($logFile.FullName) -ErrorAction Stop
+			$temp.Successful = $true
+			Write-Host "Success" -ForegroundColor Green
 		}
+		catch {
+			Write-Host "Failed" -ForegroundColor Red
+			$temp.Successful = $false
+		}
+		#Remove-Item -Path ($logFile.FullName) -Force -ErrorAction SilentlyContinue
+		$resultLog += $temp
+		}
+
 	}	
 }
-
+$resultLog | Export-Csv -NoTypeInformation $deleteLogCSVFile
 $debugLog | Out-File $debugLogFile
 
 #Region Mail
@@ -44,7 +59,7 @@ if ($SendEmail -eq $true) {
 		From = "PostMaster <Exchange-Admin@domain.com>"
 		To = "someone@domain.com"
 		SmtpServer = "smtp.server.here"
-		Attachment = $debugLogFile	
+		Attachment = $deleteLogCSVFile
 		}
 	Send-MailMessage @params
 }
